08ac66ae4b0fbed1b695393c1cb91147c5238a71
Paulo Sell <sellpaulof@gmail.com>
2025-03-19 17:59:33 -0300

    RPI5: adding support for RPI temperature monitor

---
+++ package/rpi5_hardware_monitor/rpi5_hardware_monitor.mk
@@ -0,0 +1,13 @@
+RPI5_HARDWARE_MONITOR_VERSION := 1.0.1
+RPI5_HARDWARE_MONITOR_SITE := https://github.com/paulosell/rpi-hardware-monitor.git
+RPI5_HARDWARE_MONITOR_SITE_METHOD := git
+
+define RPI5_HARDWARE_MONITOR_BUILD_CMDS
+	$(MAKE) CC=$(TARGET_CC) LD=$(TARGET_LD) AR=$(TARGET_AR) LDFLAGS=$(TARGET_LDFLAGS) -C $(@D) all 
+endef
+
+define RPI5_HARDWARE_MONITOR_INSTALL_TARGET_CMDS
+  $(INSTALL) -D -m 0755 $(@D)/src/rpi_hardware_monitor $(TARGET_DIR)/usr/bin
+endef
+
+$(eval $(generic-package))
