799830c7c6a724e129cc7a58198fedadd5a1c4bf
Paulo Sell <sellpaulof@gmail.com>
2025-03-19 18:00:19 -0300

    RPI5: fix entry in brcmfmac .mk file

--- package/brcmfmac-firmware/brcmfmac-firmware.mk
+++ package/brcmfmac-firmware/brcmfmac-firmware.mk
@@ -2,8 +2,6 @@ BRCMFMAC_FIRMWARE_VERSION := 20250311
 BRCMFMAC_FIRMWARE_SOURCE := linux-firmware-$(BRCMFMAC_FIRMWARE_VERSION).tar.gz
 BRCMFMAC_FIRMWARE_SITE := https://web.git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/snapshot
 
-BRCMFMAC_FIRMWARE_TARGET := YES
-
 define BRCMFMAC_FIRMWARE_INSTALL_TARGET_CMDS
   $(INSTALL) -D -m 0644 $(@D)/cypress/cyfmac43455-sdio.bin $(TARGET_DIR)/lib/firmware/brcm/brcmfmac43455-sdio.bin
 
